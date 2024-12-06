61e6aa772e1e3507e8bb6cfa62f684176a909327
Japhetaline <japhetdamassoh@gmail.com>
2024-12-06 17:06:20 +0100

    banking

--- src/Pages/Transactions.jsx
+++ src/Pages/Transactions.jsx
@@ -4,8 +4,8 @@ const Transfer = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
-        <div className="text-gray-400 text-[100px] font-light leading-none">$<span className="text-gray-400 text-[200px] font-light leading-none">550</span></div>
-        <p className="text-gray-400 text-[20px] font-light leading-none">Date: 15-11-2024</p>
+        <div className="text-gray-400 text-[100px] font-light leading-none"><span className="text-gray-400 text-[200px] font-light leading-none">Reloading</span></div>
+        <p className="text-gray-400 text-[20px] font-light leading-none">Date: 5-31, Dec 2024</p>
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Allow the server to be accessible via LAN
+  },
   plugins: [react()],
 })
